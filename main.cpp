ec4d7c9e592b4fed6a7df9cc5ea744b4e3d4fb84
Luna <holacarly500@gmail.com>
2023-08-09 21:32:10 +0200

    Adapted to objective

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <OpenXLSX.hpp>
+#include <vector>
 //#include <external/nowide/nowide/iostream.hpp>
 
 using namespace OpenXLSX;
+std::vector<XLCellValue> header;
 
 int main() 
 {
@@ -10,12 +12,37 @@ int main()
     doc.open("test.xlsx");
     auto wks = doc.workbook().worksheet("EQUIDAD");
     std::cout << wks.rowCount();
-    auto rows = wks.row(4);
-    auto cells = rows.cells();
-    for (auto ptr = cells.begin(); ptr != cells.end(); ptr.operator++())
+    std::ofstream db_writee("values.txt", std::ios_base::app);
+    for (uint32_t i = 1; i < wks.rowCount(); i++)
+    {
+        auto rows = wks.row(i);
+        auto cells = rows.cells();
+        if (i == 1)
+        {
+            for (auto ptr = cells.begin(); ptr != cells.end(); ptr.operator++())
+            {
+                header.push_back(ptr->value().operator OpenXLSX::XLCellValue());
+            }
+            continue;
+        }
+        int index = 0;
+        for (auto ptr = cells.begin(); ptr != cells.end(); ptr.operator++())
+        {
+            std::string comp = "CANDIDATO";
+            if (comp.compare(header[index].get<std::string>()) == 0)
+            {
+                db_writee << ptr->value().operator OpenXLSX::XLCellValue() << std::endl;
+                break;
+            }
+        }
+        
+    }
+    db_writee.close();
+
+    /*for (auto ptr = cells.begin(); ptr != cells.end(); ptr.operator++())
     {
         std::ofstream db_writee("values.txt", std::ios_base::app);
         auto type = ptr.operator*().value().type();
         db_writee << ptr->value().operator OpenXLSX::XLCellValue() << std::endl;
-    }
+    }*/
 }
