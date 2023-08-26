f3694eec867678e3decbd0fac6acf2db0f50fa7e
Luna <69774917+Lluna4@users.noreply.github.com>
2023-08-26 02:06:56 +0200

    Add files via upload

---
+++ db.hpp
@@ -0,0 +1,233 @@
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <vector>
+#include <thread>
+#include <ctime>
+#include <chrono>
+#include <fstream>
+#include <filesystem>
+#include <algorithm>
+#include <iterator>
+#include <unordered_map>
+#include <map>
+#include <format>
+#include <OpenXLSX.hpp>
+
+int	ft_atoi(const char* a)
+{
+    int	ret;
+    int	sign;
+
+    ret = 0;
+    sign = 1;
+    while ((*a >= 9 && *a <= 13) || *a == 32)
+    {
+        a++;
+    }
+    if (*a == '+' || *a == '-')
+    {
+        if (*a == '-')
+            sign = -1;
+        a++;
+    }
+    while (*a >= '0' && *a <= '9')
+    {
+        ret = ret * 10 + (*a - '0');
+        a++;
+    }
+    return (sign * ret);
+}
+
+static int	ft_intlen(int n)
+{
+	int	ret;
+
+	ret = 1;
+	while (n >= 10)
+	{
+		ret++;
+		n = n / 10;
+	}
+	return (ret);
+}
+
+static char	*ft_make_ret(int n, int sign)
+{
+	int		len;
+	char	*ret;
+
+	len = ft_intlen(n) + sign;
+	ret = (char *)calloc(len + 1, sizeof(char));
+	if (!ret)
+		return (0);
+	len--;
+	while (len >= 0)
+	{
+		ret[len] = (n % 10) + '0';
+		n = n / 10;
+		len--;
+	}
+	if (sign == 1)
+		ret[0] = '-';
+	return (ret);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*ret;
+	int		sign;
+
+	sign = 0;
+	if (n == -2147483648)
+	{
+		ret = (char *)malloc(12 * sizeof(char));
+		if (!ret)
+			return (0);
+		memcpy(ret, "-2147483648", 12);
+		return (ret);
+	}
+	if (n < 0)
+	{
+		n *= -1;
+		sign = 1;
+	}
+	return (ft_make_ret(n, sign));
+}
+
+class db
+{
+public:
+    db() {}
+
+    db(std::string name, std::vector<std::string> initial_values)
+        :name_(name), header(initial_values)
+    {}
+
+    void rename(std::string new_name)
+    {
+        name_ = new_name;
+    }
+    
+    void add_value(std::vector<std::string> new_value)
+    {
+        values.push_back(new_value);
+        for (int i = 0; i < header.size(); i++)
+        {
+            if (querying_cache.contains(header[i]) == true) 
+            {
+                if (querying_cache[header[i]].contains(new_value[i]) == true)
+                {
+                    if (overrides.contains(new_value[i]) == true)
+                    {
+                        int x = overrides[new_value[i]] + 1;
+                        overrides[new_value[i]] = x;
+                        std::cout << "A" << std::endl;
+                    }
+                    else
+                        overrides.insert({new_value[i], 1});
+                    std::cout << overrides[new_value[i]] << " " << new_value[i] << std::endl;
+                    new_value[i].append(ft_itoa(overrides[new_value[i]]));
+                }
+                querying_cache[header[i]].insert({new_value[i], {i, size}});   
+            }
+            else
+            {
+                if (header.size() < i - 1)
+                    break;
+                querying_cache.insert({header[i], {}});
+                if (querying_cache[header[i]].contains(new_value[i]) == true)
+                {
+                    if (overrides.contains(new_value[i]) == true)
+                    {
+                        int x = overrides[new_value[i]] + 1;
+                        overrides[new_value[i]] = x;
+                    }
+                    else
+                        overrides.insert({new_value[i], 1});
+                    std::cout << overrides[new_value[i]] << " " << new_value[i] << std::endl;
+                    new_value[i].append(ft_itoa(overrides[new_value[i]]));
+                }
+                querying_cache[header[i]].insert({new_value[i], {i, size}});
+            }
+        }
+        size++;
+    }
+    
+    std::vector<std::vector<std::string>> search(std::string column, std::string value)
+    {
+        std::vector<std::vector<std::string>> ret;
+        if (querying_cache.contains(column) == true)
+        {
+            if (querying_cache[column].contains(value) == true)
+            {
+                std::vector<int> pos = querying_cache[column][value];
+                std::vector<std::string> add_ve = values[pos[1]];
+                ret.push_back(add_ve);
+                if (overrides.contains(value) == true)
+                {
+                    int loops = overrides[value];
+                    std::string initial_value = value;
+                    for (int i = 1; i <= loops; i++)
+                    {
+                        value = std::format("{}{}", initial_value, ft_itoa(i));
+                        std::vector<int> posi = querying_cache[column][value];
+                        std::vector<std::string> add_vec = values[posi[1]];
+                        ret.push_back(add_vec);
+                    }
+                }
+                return ret;
+            }
+        }
+        return ret;
+    }
+
+    void remove_value(int index)
+    {
+        values.erase(values.begin() + index);
+    }
+
+    std::vector<std::vector<std::string>> get_values()
+    {
+        return values;
+    }
+
+    std::vector<std::string> get_value(int index)
+    {
+        return values[index];
+    }
+
+    std::string get_name()
+    {
+        return name_;
+    }
+
+    int get_size()
+    {
+        return size;
+    }
+
+    int header_size()
+    {
+        return header.size();
+    }
+    std::vector<std::string> get_header()
+    {
+        return header;
+    }
+
+    std::unordered_map<std::string, std::unordered_map<std::string, std::vector<int>>> cache()
+    {
+        return querying_cache;
+    }
+
+private:
+    std::string name_;
+    std::vector<std::string> header;
+    std::vector<std::vector<std::string>> values;
+    std::unordered_map<std::string, std::unordered_map<std::string, std::vector<int>>> querying_cache;
+    std::map<std::string, int> overrides;
+    int size = 0;
+
+
+};
--- main.cpp
+++ main.cpp
@@ -1,44 +1,163 @@
-#include <OpenXLSX.hpp>
-#include <vector>
+#include "db.hpp"
 //#include <external/nowide/nowide/iostream.hpp>
 
 using namespace OpenXLSX;
-std::vector<XLCellValue> header;
+std::vector<std::string> header;
+
+std::vector<db> dbs;
+
+void print_line(std::vector<std::string> header)
+{
+    for (unsigned int i = 0; i < header.size(); i++)
+    {
+        std::cout << "+";
+        std::cout << "-";
+        for (unsigned int x = 0; x < header[i].size(); x++)
+            std::cout << "-";
+        std::cout << "-";
+    }
+    std::cout << "+";
+    std::cout << std::endl;
+}
+
+void print_line(std::vector<std::string> header, std::vector<int> max)
+{
+    for (unsigned int i = 0; i < header.size(); i++)
+    {
+        std::cout << "+";
+        std::cout << "-";
+        for (unsigned int x = 0; x < max[i]; x++)
+            std::cout << "-";
+        std::cout << "-";
+    }
+    std::cout << "+";
+    std::cout << std::endl;
+}
+
+void print_words(std::vector<std::string> header)
+{
+    for (unsigned int i = 0; i < header.size(); i++)
+    {
+        std::cout << "|" << " " << header[i] << " ";
+    }
+    std::cout << "|";
+    std::cout << std::endl;
+}
+
+void print_words(std::vector<std::string> header, std::vector<int> max)
+{
+    for (unsigned int i = 0; i < header.size(); i++)
+    {
+        std::cout << "|" << " " << header[i];
+        for (unsigned int x = 0; x < (max[i] - (header[i].length() - 1)); x++)
+            std::cout << " ";
+    }
+    std::cout << "|";
+    std::cout << std::endl;
+}
+
+void show()
+{
+    std::vector<int> max;
+    std::vector<std::vector<std::string>> values = dbs[0].get_values();
+    std::vector<std::string> header = dbs[0].get_header();
+    for (unsigned int y = 0; y < header.size(); y++)
+    {
+        max.push_back(header[y].length());
+        for (unsigned int x = 0; x < values.size(); x++)
+        {
+            if (max.empty() || (max.size() - 1) < y)
+            {
+                max.push_back(values[x][y].size());
+                continue;
+            }
+            if (values[x][y].size() > max[y])
+                max[y] = values[x][y].size();
+        }
+    }
+    print_line(header, max);
+    print_words(header, max);
+    print_line(header, max);
+    for (unsigned int y = 0; y < values.size(); y++)
+    {
+        print_words(values[y], max);
+        print_line(values[y], max);
+    }
+
+    return;
+}
+
 
 int main() 
 {
 
     XLDocument doc;
+    XLDocument write;
+    write.create("output.xlsx");
     doc.open("test.xlsx");
-    auto wks = doc.workbook().worksheet("EQUIDAD");
+    write.workbook().addWorksheet("EQUIDAD");
+    auto wkb = write.workbook().worksheet("EQUIDAD");
+    auto wks = doc.workbook().worksheet("Hoja1");
     std::cout << wks.rowCount();
-    std::ofstream db_writee("values.txt", std::ios_base::app);
-    for (uint32_t i = 1; i < wks.rowCount(); i++)
+    //db_writee.close();
+    write.save();
+    uint16_t col = 1;
+    uint32_t row = 1;
+    for (uint32_t i = 1; i < 1000; i++)
     {
+        col = 1;
         auto rows = wks.row(i);
         auto cells = rows.cells();
-        if (i == 1)
+        if (i < 14)
+            continue;
+        if (i == 14)
         {
             for (auto ptr = cells.begin(); ptr != cells.end(); ptr.operator++())
             {
-                header.push_back(ptr->value().operator OpenXLSX::XLCellValue());
+                if (ptr->value().operator OpenXLSX::XLCellValue().get<std::string>().compare("ITEM") == 0)
+                    std::cout << ptr->value().typeAsString() << ptr->value().operator OpenXLSX::XLCellValue() << std::endl;
+                if (ptr->value().typeAsString().compare("empty") == 0)
+                    continue;
+                header.push_back(ptr->value().operator OpenXLSX::XLCellValue().get<std::string>());
+                //std::cout << ptr->value().typeAsString() << ptr->value().operator OpenXLSX::XLCellValue() << std::endl;
             }
-            continue;
+            dbs.push_back(db("tabla", header));
         }
-        int index = 0;
+        int a = dbs[0].get_header().size();
+        std::cout << a<< std::endl;
+        
+        int index = 18;
+        int i2 = 0;
+        std::vector<std::string> values;
         for (auto ptr = cells.begin(); ptr != cells.end(); ptr.operator++())
         {
-            std::string comp = "CANDIDATO";
-            if (comp.compare(header[index].get<std::string>()) == 0)
-            {
-                db_writee << ptr->value().operator OpenXLSX::XLCellValue() << std::endl;
+            if (i2 == a)
                 break;
+            if (ptr->value().operator OpenXLSX::XLCellValue().typeAsString().compare("integer") == 0)
+            {
+                values.push_back(ft_itoa(ptr->value().operator OpenXLSX::XLCellValue()));
             }
+            if (ptr->value().typeAsString().compare("empty") == 0)
+            {
+                values.push_back("");
+            }
+            if (ptr->value().operator OpenXLSX::XLCellValue().typeAsString().compare("string") == 0)
+            {
+                values.push_back(ptr->value().operator OpenXLSX::XLCellValue().get<std::string>());
+            }
+            
+            //wkb.cell(XLCellReference(row, col)).value() = ptr->value().operator OpenXLSX::XLCellValue();
+            col++;
+            i2++;
         }
-        
+        std::cout << values.size() << std::endl;
+        dbs[0].add_value(values);
+        row++;
     }
-    db_writee.close();
-
+    std::cout << dbs[0].get_size() << std::endl;
+    show();
+    write.save();
+    write.close();
     /*for (auto ptr = cells.begin(); ptr != cells.end(); ptr.operator++())
     {
         std::ofstream db_writee("values.txt", std::ios_base::app);
